53018611326ab7ff6c789e542f0270fc2260a7e9
LvBingru <lvbingru@gmail.com>
2015-11-25 14:39:45 +0800

    path map view

--- src/PathMapView.js
+++ src/PathMapView.js
@@ -2,23 +2,26 @@
  * Created by lvbingru on 11/25/15.
  */
 
-import React, {InteractionManager, Component, PropTypes, View, Text, requireNativeComponent, NativeModules,StyleSheet} from 'react-native';
+import React, {Image, Component, PropTypes, View, Text, requireNativeComponent, NativeModules,StyleSheet, TouchableOpacity} from 'react-native';
 const BaiduMap = requireNativeComponent('RCTBaiduMap');
 const BaiduLocation = NativeModules.BaiduLocationObserver;
+const su = require('./styleUtils');
+const icons = require('./icons');
 
 const propTypes = {}
 
 const defaultProps = {}
 
-export default class PathMapView extends Component {
+class PathMapView extends Component {
     constructor(props) {
         super(props);
 
       let locationArray = props.locationArray;
 
       // ---test---
-      if (locationArray == nil) {
+      if (!locationArray) {
         BaiduLocation.loadRecordLocation((r)=>{
+          console.log(r);
           this.setState({
             locationArray : r.locations,
           })
@@ -50,3 +53,44 @@ var styles = StyleSheet.create({
   },
 
 });
+
+var BaseRouteMapper = require('./BaseRouteMapper');
+
+class Route extends BaseRouteMapper {
+
+  constructor(params) {
+    super();
+    params = params || {};
+    this.params = params;
+  }
+
+  get title() {
+    return 'xxx的足迹';
+  }
+
+  renderLeftButton(route, navigator, index, navState) {
+    return this._renderBackButton(route, navigator, index, navState);
+  }
+
+  renderRightButton(route, navigator, index, navState) {
+    var styles = this.styles;
+    var icon = {
+      ...su.size(18, 18),
+    };
+
+    return (
+      <TouchableOpacity
+        style={[styles.wrap, styles.right]}>
+        <Image style={icon} source={icons.share}/>
+      </TouchableOpacity>
+    );
+  }
+
+  renderScene() {
+    return (
+      <PathMapView {...this.params}/>
+    );
+  }
+}
+
+module.exports = Route;
